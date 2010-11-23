0281999cc71d87dee22678dca00f7e90e8a8c0f0
Natan Yellin <aantny@gmail.com>
2010-11-24 01:32:25 +0200

    refactored code: * break code down into functions * other minor changes

--- drupal_coverage.py
+++ drupal_coverage.py
@@ -13,51 +13,80 @@
 import os
 import argparse
 
-parser = argparse.ArgumentParser(description="Test statistics for Drupal Core Modules. This script checks out" +
-				" all of the drupal core modules and calculates each module's ratio of [lines of code]" +
-				" / [unit tests]")
+# Define constants
+CODE_EXTS = ["php", "inc", "module", "install"]	# which file extensions are included in the linecount
 
-# add an argument to use an existing drupal checkout
-#parser.add_argument("-c", "--checkout-dir", dest="checkout", default="modules",
-#	help="the directory where we'll checkout drupal modules")
+# Setup command line arguments
+parser = argparse.ArgumentParser(description="Test statistics for Drupal Core Modules. This script checks out all of the drupal"
+				+ "core modules and calculates each module's ratio of [lines of code] / [unit tests]")
+#parser.add_argument("-c", "--checkout-dir", dest="checkout", default="modules", help="the directory where we'll checkout drupal modules")
 args = parser.parse_args()
 
-# The following file extensions are included in the linecount
-exts = ["php", "inc", "module", "install"]
+# Functions
+def print_header ():
+	"""Prints the titles of each output column"""
+	# these are titles of each output column
+	cols = ["Module", "Code lines", "Test lines", "Asserts", "Ratio * 100"]
+	
+	# take each title and pad it with whitespace (so that each column has a standard width)	
+	outp = ""
+	for col in cols:
+		outp += col.ljust(10) + "\t"
 
-# Download all drupal core modules from CVS
-os.system("cvs -z6 -d:pserver:anonymous:anonymous@cvs.drupal.org:/cvs/drupal checkout -P drupal/modules")
+	# print out the header
+	print "-" * 80	
+	print outp
+	print "-" * 80
 
-# Get a list of all modules
-modules = os.listdir("drupal/modules")
-for r in ["README.txt", "CVS"]:
-	modules.remove(r)
- 
-# Loop over modules
-print_header()
-for mod in modules:
+def download_core_modules ():
+	"""Downloads all core drupal modules from CVS and returns a list of paths to each module"""
+	# Download all core modules to ./drupal/modules/
+	os.system("cvs -z6 -d:pserver:anonymous:anonymous@cvs.drupal.org:/cvs/drupal checkout -P drupal/modules")
+	# Return the paths of each module
+	FILTER = ["README.txt", "CVS"]
+	return [os.path.abspath("drupal/modules/" + module)
+		for module in os.listdir("drupal/modules")
+		if module not in FILTER]
+
+def calc_module_stats (module):
+	"""
+	Calculates and prints stats for one module.
+	
+	module: the module directory
+	"""
 	# setup stats for this module
 	lines = 0
 	test_lines = 0
 	test_asserts = 0
 	
+	# get the module's name from it's path
+	name = module.split("/")[-1]
+
 	# loop over all files in the module
-	for f in files = os.listdir("drupal/modules/" + mod):
-		# get the file extension and check code type
+	for f in os.listdir(module):
+		# get the file's extension
 		ext = f.split(".")[-1]
-		if ext == "test":	# this is test code
-			for line in open("drupal/modules/" + mod + "/" + f):
+		
+		# get the file's full path
+		path = os.path.join(module, f)
+		
+		# if the file is a test case
+		if ext == "test":
+			for line in open(path):
 				test_lines += 1
 				test_asserts += line.count("assert")
-		elif ext in exts:	# this is the module's php code
-			for line in open("drupal/modules/" + mod + "/" + f): lines += 1
+		# if the file is php code
+		elif ext in CODE_EXTS:
+			for line in open(path): lines += 1
+	
 	# print out stats for this module
-	print "%s\t%10d\t%10d\t%10d\t%4f" % (mod.ljust(10), lines, test_lines, test_asserts, float(test_asserts) / lines * 100)
+	print "%s\t%10d\t%10d\t%10d\t%4f" % (name.ljust(10), lines, test_lines, test_asserts,
+						float(test_asserts) / lines * 100)
 
-def print_header ():
-	"""Prints the titles of each output column"""
-	print "-" * 80
-	print "%s\t%s\t%s\t%s\t%s" %	("Module".ljust(10), "Code lines".ljust(10), "Test lines".ljust(10),
-					 "Asserts".ljust(10), "Ratio * 100".ljust(10))
-	print "-" * 80
+# Main program
+if __name__ == "__main__":
+	modules = download_core_modules()
+	print_header()
+	for mod in modules:
+		calc_module_stats(mod)	
 
